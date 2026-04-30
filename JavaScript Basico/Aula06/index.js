9a970af4a9280af5029bda5d4da603fb3cc452b3
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2026-04-30 01:01:38 -0300

    imprimindo valores

---
+++ JavaScript Basico/Aula06/index.js
@@ -0,0 +1,14 @@
+ const nome = "Livia";
+ const sobrenome = "Silva";
+
+ const idade = 22;
+ const peso = 88;
+
+ const altura = 4.50;
+
+ let imc;
+ let anoNasc;
+
+ imc = peso / (altura * altura);
+ anoNasc = 2026 - idade
+ console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso, 'kg');
